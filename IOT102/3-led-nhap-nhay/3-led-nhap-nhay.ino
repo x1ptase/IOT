b1f49cff11130176b278ee2af94bde68603387cd
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-10-02 13:28:54 +0700

    update src

---
+++ IOT102/3-led-nhap-nhay/3-led-nhap-nhay.ino
@@ -0,0 +1,29 @@
+int led1=13;
+int led2=12;
+int led3=11; 
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  // Bật LED1, tắt LED2 và LED3
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  delay(1000);
+
+  // Bật LED2, tắt LED1 và LED3
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  delay(1000);
+
+  // Bật LED3, tắt LED1 và LED2
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+}
